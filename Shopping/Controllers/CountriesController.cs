56c52424457b4c7c70c93a379afe38c5549418b7
DayhanaRamirez <dayhanaramirez45@gmail.com>
2022-03-08 22:37:54 -0500

    Modified create, details, edit. Add sort, search, pagination.

--- Shopping/Controllers/CountriesController.cs
+++ Shopping/Controllers/CountriesController.cs
@@ -1,10 +1,5 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Shopping.Data;
 using Shopping.Data.Entities;
@@ -55,7 +50,7 @@ namespace Shopping.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Country country)
+        public async Task<IActionResult> Create( Country country)
         {
             if (ModelState.IsValid)
             {
